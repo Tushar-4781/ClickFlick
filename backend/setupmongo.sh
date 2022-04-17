41aeb12f4d79a1cb5e31c78ee8331224d751c532
Tushar Inani <39093015+Tushar-4781@users.noreply.github.com>
2022-04-18 02:07:21 +0530

    Delete setupmongo.sh

--- backend/setupmongo.sh
+++
@@ -1,1 +0,0 @@
-mongoimport --uri "mongodb+srv://Tushar4781:6674781rias@xflix-node.rcjzh.mongodb.net/xflix?retryWrites=true&w=majority" --drop --collection videos --file data/export_xflix_videos.json
